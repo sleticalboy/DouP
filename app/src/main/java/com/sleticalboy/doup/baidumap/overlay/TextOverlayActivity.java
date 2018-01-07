c7f30eef9fac8cfb5b733239c59ba7c065ff4755
sleticalboy <sleticalboy@gmail.com>
2018-01-07 14:13:27 +0800

    地图-添加不同的图层

---
+++ app/src/main/java/com/sleticalboy/doup/baidumap/overlay/TextOverlayActivity.java
@@ -0,0 +1,32 @@
+package com.sleticalboy.doup.baidumap.overlay;
+
+import android.graphics.Color;
+
+import com.baidu.location.BDLocation;
+import com.baidu.mapapi.map.TextOptions;
+import com.sleticalboy.doup.baidumap.BaseMapActivity;
+import com.sleticalboy.doup.baidumap.location.LocationManager;
+
+/**
+ * Created by Android Studio.
+ * Date: 1/7/18.
+ *
+ * @author sleticalboy
+ */
+public class TextOverlayActivity extends BaseMapActivity {
+    @Override
+    protected void handleLocation(BDLocation location) {
+        animate2tam();
+    }
+
+    @Override
+    protected void init() {
+        TextOptions options = new TextOptions()
+                .position(LocationManager.TAM)  // 位置
+                .text("sleticalboy")            // 文本
+                .fontColor(Color.RED)           // 颜色
+                .fontSize(50)                   // 字号
+                .rotate(45);                    // 旋转角度
+        mBaiduMap.addOverlay(options);
+    }
+}
